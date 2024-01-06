fbb0f37a784dfb44eb8d740b11b4e0303b35dd65
yaelahbarr <wdeaddgaming@gmail.com>
2024-01-06 15:38:41 +0700

    menambahkan user.java di package user

---
+++ src/user/User.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package user;
+
+/**
+ *
+ * @author wdead
+ */
+public class User {
+    
+    private String id;
+     private String nama;
+    private String email;
+    private String password;
+   
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public String getId() {
+        return this.id;
+    }
+    
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+    public String getNama() {
+        return this.nama;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getEmail() {
+        return this.email;
+    }
+    
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    public String getPassword() {
+        return this.password;
+    }
+    
+}
